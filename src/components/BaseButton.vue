4a22e82e53d45031c3018cab475fa517143a88de
Sam <samuv.eth@gmail.com>
2022-05-06 20:43:41 +0700

    Add first test for BaseButton

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts"></script>
-
 <template>
   <button
     type="button"
-    class="rounded-md text-zinc-50 focus:outline-none focus:ring-indigo-300/50 focus-visible:ring-2"
+    class="rounded-md focus:outline-none focus:ring-indigo-300/50 focus-visible:ring-2"
   >
     <slot />
   </button>
