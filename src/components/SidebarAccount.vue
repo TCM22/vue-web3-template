c28272d3a0d0ef7a3c33922392f348f6ef6104ee
Sam <samuv.eth@gmail.com>
2022-05-28 19:44:11 +0700

    Fixes

--- src/components/SidebarAccount.vue
+++ src/components/SidebarAccount.vue
@@ -9,20 +9,21 @@ const { web3Account, web3 } = useWeb3()
   <div
     class="flex w-full flex-shrink-0 items-center rounded-lg p-2 text-left hover:bg-zinc-700"
   >
-    <div>
-      <img
-        class="inline-block h-12 w-12 rounded-full"
-        :src="`https://stamp.fyi/avatar/eth:${web3Account}?s=${
-          parseInt('100') * 2
-        }`"
-        alt="web3-avatar"
-      />
-    </div>
-    <div class="ml-3">
+    <img
+      class="inline-block h-12 w-12 rounded-full"
+      :src="`https://stamp.fyi/avatar/eth:${web3Account}?s=${
+        parseInt('100') * 2
+      }`"
+      alt="web3-avatar"
+    />
+
+    <div class="ml-3 truncate">
       <p class="text-sm font-medium text-zinc-200">
         {{ shortenAddress(web3Account) }}
       </p>
-      <p class="text-xs font-medium text-zinc-400">{{ web3.ens }}</p>
+      <p class="min-h-[16px] truncate text-xs font-medium text-zinc-400">
+        {{ web3.ens }}
+      </p>
     </div>
   </div>
 </template>
